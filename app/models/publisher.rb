b2d0afd8abf7336a567b0f082a3c7eb928657edd
Zhiguang Xu <xuzhiguang@gmail.com>
2013-03-08 12:06:46 -0500

    Now Publishers can be managed.

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,3 +1,5 @@
 class Publisher < ActiveRecord::Base
-  has_many :products
+  has_many :products, dependent: :destroy
+  
+  validates_presence_of :name
 end
---
+++ test/functional/publisher_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PublisherControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
